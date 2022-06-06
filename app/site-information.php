23ebe088a35a5c43b4f750874be12387ac4f3605
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-06-06 20:11:58 +0900

    final project day -5

---
+++ app/site-information.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $site_title = "Highland Roastery";
+    $site_description = "Roasting House and Coffee Shop in Papua";
+    $tag_line = "Good Farmer - Best Coffee - Better Quality";
+
+    $site_logo = "/assets/img/logo-hr.png";
+
+    $copyright_info = "Copyright &copy; 2022";
+    $author = "Janzen Faidiban";
+
+    $instagram = "https://www.instagram.com/highlandroastery/";
+    $facebook = "https://facebook.com/";
+    $youtube = "https://youtube.com/";
